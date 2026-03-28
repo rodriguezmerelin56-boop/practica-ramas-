b350b5de44eec8c484d9877a5cb73034149855c0
rodriguezmerelin56-boop <rodriguezmerelin56@gmail.com>
2026-03-28 14:42:31 -0500

    Update personal information in datos.txt

--- texto 1.py
+++ texto 1.py
@@ -3,8 +3,8 @@
 
 # Crear y escribir en un archivo
 with open("datos.txt", "w") as archivo:
-    archivo.write("Nombre: Juan Pérez\n")
-    archivo.write("Edad: 25\n")
+    archivo.write("Nombre: Jose \n")
+    archivo.write("Edad: 30\n")
     archivo.write("País: República Dominicana\n")
 
 print("Archivo creado y datos guardados correctamente.")
